37a36d1cf583aabda7e96b7b43f33f7c07a9118a
Todd Short <todd.short@me.com>
2025-04-16 15:48:18 -0400

    TEMP: add-local-catalog-image
    Signed-off-by: Todd Short <todd.short@me.com>

--- test/extended/util/annotate/generated/zz_generated.annotations.go
+++ test/extended/util/annotate/generated/zz_generated.annotations.go
@@ -1923,6 +1923,8 @@ var Annotations = map[string]string{
 
 	"[sig-olmv1][OCPFeatureGate:NewOLM][Skipped:Disconnected] OLMv1 operator installation should install a cluster extension": " [Suite:openshift/conformance/parallel]",
 
+	"[sig-olmv2][OCPFeatureGate:NewOLM] OLMv1 local operator installation should create a catalog": " [Suite:openshift/conformance/parallel]",
+
 	"[sig-operator] OLM should Implement packages API server and list packagemanifest info with namespace not NULL [apigroup:packages.operators.coreos.com]": " [Skipped:NoOptionalCapabilities] [Suite:openshift/conformance/parallel]",
 
 	"[sig-operator] OLM should be installed with catalogsources at version v1alpha1 [apigroup:operators.coreos.com]": " [Skipped:NoOptionalCapabilities] [Suite:openshift/conformance/parallel]",
